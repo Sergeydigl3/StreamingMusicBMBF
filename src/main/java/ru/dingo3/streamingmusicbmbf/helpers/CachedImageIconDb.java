cce589eabe77923d86910d74b3e11a1ac5eef2f3
Sergeydigl3-PC <26508358+Sergeydigl3@users.noreply.github.com>
2023-07-12 09:41:56 +0300

    Fixed Charset for cyrillic

--- src/main/java/ru/dingo3/streamingmusicbmbf/helpers/CachedImageIconDb.java
+++ src/main/java/ru/dingo3/streamingmusicbmbf/helpers/CachedImageIconDb.java
@@ -8,15 +8,24 @@ import java.nio.file.*;
 public class CachedImageIconDb {
 
     private final String path;
+
     public CachedImageIconDb(String path) {
         this.path = path;
     }
 
     public ImageIcon getByUrl(String url) {
-        Path file = Paths.get(path + url.hashCode() + ".png");
+        Path file = Paths.get(path, "cache", url.hashCode() + ".jpg");
+//        System.out.println("Проверяем файл " + file);
         if (Files.exists(file)) {
             return new ImageIcon(file.toString());
         } else {
+            if (!Files.exists(file.getParent())) {
+                try {
+                    Files.createDirectories(file.getParent());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
             try (InputStream in = new URL(url).openStream()) {
                 Files.copy(in, file);
                 System.out.println("Скачали файл " + file);
@@ -24,6 +33,14 @@ public class CachedImageIconDb {
             } catch (Exception e) {
                 e.printStackTrace();
             }
+        }
+        return null;
+    }
+
+    public ImageIcon getByUrlResized(String url, int width, int height) {
+        ImageIcon imageIcon = getByUrl(url);
+        if (imageIcon != null) {
+            return new ImageIcon(imageIcon.getImage().getScaledInstance(width, height, java.awt.Image.SCALE_SMOOTH));
         }
         return null;
     }
